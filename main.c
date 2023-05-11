a8e185e49d53f9c2450230acf0c5a2c9744a51ee
XK <47260768+XK9274@users.noreply.github.com>
2023-05-11 16:09:50 +0100

    Updated to add antialiasing to text
    Updated to add antialiasing to text

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
+#include <SDL/SDL_gfxPrimitives.h>
+#include <SDL/SDL_rotozoom.h>
 
 #include <fcntl.h>
 #include <linux/fb.h>
@@ -11,98 +13,6 @@
 #include <stdio.h>
 
 #define onionFont "/mnt/SDCARD/miyoo/app/Exo-2-Bold-Italic.ttf"
-
-static void blit(void* _dst, int dst_w, int dst_h, void* _src, int src_w, int src_h, int ox, int oy) {
-    uint8_t* dst = (uint8_t*)_dst;
-    uint8_t* src = (uint8_t*)_src;
-
-    for (int y=0; y<src_h; y++) {
-        uint8_t* dst_row = dst + (((((dst_h - 1 - oy) - y) * dst_w) - 1 - ox) * 4);
-        uint8_t* src_row = src + ((y * src_w) * 4);
-        for (int x=0; x<src_w; x++) {
-            float a = *(src_row+3) / 255.0;
-            if (a>0.8) {
-                *(dst_row+0) = *(src_row+0) * a;
-                *(dst_row+1) = *(src_row+1) * a;
-                *(dst_row+2) = *(src_row+2) * a;
-                *(dst_row+3) = 0xff;
-            }
-            dst_row -= 4;
-            src_row += 4;
-        }
-    }
-}
-
-Uint32 get_pixel(SDL_Surface* surface, int x, int y);
-void put_pixel(SDL_Surface* surface, int x, int y, Uint32 pixel);
-
-SDL_Surface* flip_surface(SDL_Surface* src) {
-    SDL_Surface* dest = SDL_CreateRGBSurface(SDL_SWSURFACE, src->w, src->h, src->format->BitsPerPixel, src->format->Rmask, src->format->Gmask, src->format->Bmask, src->format->Amask);
-    if (!dest) {
-        return NULL;
-    }
-
-    for (int y = 0; y < src->h; ++y) {
-        for (int x = 0; x < src->w; ++x) {
-            int flipped_x = src->w - x - 1;
-            int flipped_y = src->h - y - 1;
-            Uint32 pixel = get_pixel(src, x, y);
-            put_pixel(dest, flipped_x, flipped_y, pixel);
-        }
-    }
-
-    return dest;
-}
-
-Uint32 get_pixel(SDL_Surface* surface, int x, int y) {
-    int bpp = surface->format->BytesPerPixel;
-    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
-
-    switch (bpp) {
-        case 1:
-            return *p;
-        case 2:
-            return *(Uint16*)p;
-        case 3:
-            if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-                return p[0] << 16 | p[1] << 8 | p[2];
-            } else {
-                return p[0] | p[1] << 8 | p[2] << 16;
-            }
-        case 4:
-            return *(Uint32*)p;
-        default:
-            return 0;
-    }
-}
-
-void put_pixel(SDL_Surface* surface, int x, int y, Uint32 pixel) {
-    int bpp = surface->format->BytesPerPixel;
-    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
-
-    switch (bpp) {
-        case 1:
-            *p = pixel;
-            break;
-        case 2:
-            *(Uint16*)p = pixel;
-            break;
-        case 3:
-            if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-                p[0] = (pixel >> 16) & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = pixel & 0xff;
-            } else {
-                p[0] = pixel & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = (pixel >> 16) & 0xff;
-            }
-            break;
-        case 4:
-            *(Uint32*)p = pixel;
-            break;
-    }
-}
 
 int main(int argc, char *argv[]) {
     if (argc != 7) {
@@ -116,8 +26,6 @@ int main(int argc, char *argv[]) {
     char* color_str = argv[3];
     int font_size = atoi(argv[4]);
 
-
-	// Determine the color to use
 	SDL_Color color;
 	if (strcmp(color_str, "white") == 0) {
 		color = (SDL_Color) {0xff, 0xff, 0xff};
@@ -148,14 +56,12 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 		
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Error: Unable to initialize SDL: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
     atexit(SDL_Quit);
 
-    // Initialize TTF
     if (TTF_Init() != 0) {
         printf("Error: Unable to initialize SDL_ttf: %s\n", TTF_GetError());
         return EXIT_FAILURE;
@@ -166,11 +72,13 @@ int main(int argc, char *argv[]) {
 	char* path = NULL;
 	if (access(onionFont, F_OK) == 0) path = onionFont;
 	if (!path) return 0;
-	TTF_Font* font = TTF_OpenFont(path, font_size); // Set font size to 40 pixels
+	TTF_Font* font = TTF_OpenFont(path, font_size);
 	if (!font) {
 		printf("Error: Unable to load font: %s\n", TTF_GetError());
 		return EXIT_FAILURE;
 	}
+	
+	TTF_SetFontHinting(font, TTF_HINTING_NORMAL);
 
 	// Render the text
 	SDL_Surface* text = TTF_RenderText_Blended(font, text_str, color);
@@ -180,10 +88,6 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-    // Set the surface alpha blending mode
-    SDL_SetAlpha(text, SDL_SRCALPHA, SDL_ALPHA_OPAQUE);
-
-    // Open the framebuffer device
     int fb_fd = open("/dev/fb0", O_RDWR);
     if (fb_fd == -1) {
         perror("Error: Unable to open framebuffer device");
@@ -192,7 +96,6 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Get framebuffer information
     struct fb_var_screeninfo vinfo;
     if (ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
         perror("Error: Unable to get variable screen information");
@@ -202,8 +105,7 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Create an SDL surface from the framebuffer
-    int width = vinfo.xres;
+	int width = vinfo.xres;
     int height = vinfo.yres;
     int bpp = vinfo.bits_per_pixel;
     int pitch = width * (bpp / 8);
@@ -211,34 +113,34 @@ int main(int argc, char *argv[]) {
     void* fb0_map = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
     SDL_Surface* screen = SDL_CreateRGBSurfaceFrom(fb0_map, width, height, bpp, pitch, 0, 0, 0, 0);
 
-	// Render the text onto the framebuffer surface
 	int x = atoi(argv[5]);
-	int y = atoi(argv[6]);
-	SDL_Rect dst_rect = {x, y, text->w, text->h};
+    int y = atoi(argv[6]);
+    SDL_Rect dst_rect = {x, y, text->w, text->h};
 
-	SDL_Surface* flipped_text = flip_surface(text);
-	if (!flipped_text) {
-		fprintf(stderr, "Error: Unable to flip the text: %s\n", SDL_GetError());
-		SDL_FreeSurface(text);
+	SDL_Surface* rotated_text = rotozoomSurface(text, 180, 1, 1);
+	if (!rotated_text) {
+        fprintf(stderr, "Error: Unable to rotate the text: %s\n", SDL_GetError());
+        SDL_FreeSurface(text);
 		TTF_CloseFont(font);
 		return EXIT_FAILURE;
 	}
-	SDL_FreeSurface(text);
-	text = flipped_text;
+    SDL_FreeSurface(text);
+    text = rotated_text;
 
 	Uint32 start_time = SDL_GetTicks();
-	Uint32 elapsed_time = 0;
+    Uint32 elapsed_time = 0;
 
-	while (elapsed_time < duration) {
-		SDL_BlitSurface(text, NULL, screen, &dst_rect);
-		elapsed_time = SDL_GetTicks() - start_time;
+    while (elapsed_time < duration) {
+    SDL_BlitSurface(text, NULL, screen, &dst_rect);
+    elapsed_time = SDL_GetTicks() - start_time;
+    SDL_Delay(1000);
 	}
 
-	// Clean up
-	munmap(fb0_map, map_size);
-	close(fb_fd);
-	SDL_FreeSurface(text);
-	TTF_CloseFont(font);
+    // Clean up
+    munmap(fb0_map, map_size);
+    close(fb_fd);
+    SDL_FreeSurface(text);
+    TTF_CloseFont(font);
 
-	return 0;
+    return 0;
 }
--- makefile
+++ makefile
@@ -3,12 +3,15 @@ $(error missing CROSS_COMPILE for this toolchain)
 endif
 
 TARGET = texpop
+CC = $(CROSS_COMPILE)gcc 
+CFLAGS   = -marm -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7ve
+LDFLAGS  = -Wl,-rpath-link=/opt/miyoomini-toolchain/arm-linux-gnueabihf/libc/usr/lib
+LIBS     = -lSDL -lSDL_image -lSDL_ttf -lSDL_gfx
 
-CC = $(CROSS_COMPILE)gcc
-CFLAGS   = -marm -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7ve 
-LDFLAGS	 = -lSDL -lSDL_image -lSDL_ttf
+all: $(TARGET)
 
-all:
-	$(CC) main.c -o $(TARGET) $(CFLAGS) $(LDFLAGS)
+$(TARGET): main.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $(TARGET) main.c $(LIBS)
+
 clean:
 	rm -f $(TARGET)
